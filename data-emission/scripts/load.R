2a02e96c264033e955898e13745c1921f3504831
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2024-09-16 08:20:40 +0200

    updating path for downloading data from ALFAM2 database

--- data-emission/scripts/load.R
+++ data-emission/scripts/load.R
@@ -1,6 +1,6 @@
 
 # Data not yet released, reading from the following commit: 
-ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/f131ad66f5879aece135278f6fdc3edfd97b35e2/'
+ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/0098212e52b3a6b8569ac0a080213cf65ae72374/'
 rtag <- ''
 
 p <- paste0(ghpath, rtag)
