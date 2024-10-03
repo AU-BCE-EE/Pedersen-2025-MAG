5fc90e0d334f7e3c21eede141d624922bd6d5364
Sasha D. Hafner <sasha@hafnerconsulting.com>
2024-10-03 13:23:14 -0400

    Try to fix issue #7.
    Use 2/4 digit year to identify different date formats.

--- scripts-temp-loggers/subset.R
+++ scripts-temp-loggers/subset.R
@@ -8,8 +8,24 @@ colnames(ndat)[2] <- 'temp'
 
 # temperature logger data: 
 tdat <- rbind(t1, t2, t3, t4)
-tdat$t.start <- as.POSIXct(tdat$date.time, format = '%m-%d-%Y %H:%M:%S', tz = 'UTC')
-# tdat <- tdat[, c('temp', 'pos', 't.start')]
+
+# Date problem: some as dd-mm-yy and some mm-dd-yyyy
+# Luckily they can be told apart by number of year digits! (Otherwise, it's back to the source to correct.)
+# Correct by identifying which rows have which by date length, correct, then convert separately
+tdat[, date := substr(date.time, 1, 10)]
+tdat[, date := gsub(' [0-9]$', '', date)]
+tdat[, date.len := nchar(date)]
+
+# Create empty t.start column
+tdat[, t.start := NA + Sys.time()]
+# Separately convert dates and fill into new column
+tdat[date.len == 8,  t.start := as.POSIXct(date.time, format = '%d-%m-%y %H:%M:%S', tz = 'UTC')]
+tdat[date.len == 10, t.start := as.POSIXct(date.time, format = '%m-%d-%Y %H:%M:%S', tz = 'UTC')]
+
+# Check for missing dates
+if (any(is.na(tdat$t.start))) stop('Date problem')
+
+#ggplot(tdat, aes(t.start, temp, colour = log.ID)) + geom_line()
 
 
 ggplot(ndat, aes(t.start, temp)) + geom_point()
