693c5884315bb4d963457a101a64d8c3361dbb7f
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2024-10-21 13:03:24 +0200

    adding DFC key to d2 ('plot' colum)

--- scripts-temp-loggers/main.R
+++ scripts-temp-loggers/main.R
@@ -6,7 +6,6 @@ source('load.R')
 source('datetime.R')
 source('calc.R')
 source('subset.R')
-source('func.R')
 source('plot.R')
 render('temp_stats.Rmd', output_dir = '../stats-temp')
 
--- scripts-temp-loggers/subset.R
+++ scripts-temp-loggers/subset.R
@@ -1,16 +1,16 @@
 
 
-ndat <- subset(idat, select = c('t.start', 'air.temp'))
+ndat <- subset(idat, select = c('t.start', 'air.temp', 'plot'))
 ndat$pos <- 'ambient'
 ndat$log.ID <- 'ambient'
 colnames(ndat)[2] <- 'temp'
 
 # dataframe for plotting time vs temp
-d1 <- rbind(tdat, ndat)
+d1 <- rbind(tdat, ndat[, c(1,2,4,5)])
 
 # data frame for plotting the differen temperatures against each other and investigating env effects. 
 d2 <- merge(tsummI, tsummO, by = 't.start')
-d2 <- merge(d2, ndat[ , 1:2], by = 't.start')
+d2 <- merge(d2, ndat[ , 1:3], by = 't.start')
 d2 <- merge(d2, clim, by = 't.start')
 
 
